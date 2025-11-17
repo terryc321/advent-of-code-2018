369074bc956f259f0d8d91cba415ca53c90c270e
terry <terry.cadd@gmail.com>
2025-11-17 23:41:13 +0000

    Ok some fixed antlr clojure again
    i think had to do with parser
    did not like being assigned to a variable
    possible the parser defintion was also broken
    
    (map parser (list "NSEW"))
    got it to compile
    ever since then it seems ok

--- day20/clojure/day20/project.clj
+++ day20/clojure/day20/project.clj
@@ -1,10 +1,16 @@
+
+#_( this hash underscore means ignore following s expression )
+
 (defproject day20 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.3"]
-                 [clj-antlr "0.2.14"]]
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [clj-commons/pomegranate "1.2.25"] #_(this is for finding those pesky jar files)
+                 [com.fzakaria/slf4j-timbre "0.4.1"] #_(this is for silencing logger message errors)
+                 [clj-antlr "0.2.14"] #_(this is for the parsing library )
+                 ]
   :main ^:skip-aot day20.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- day20/clojure/day20/src/day20/core.clj
+++ day20/clojure/day20/src/day20/core.clj
@@ -1,16 +1,33 @@
 (ns day20.core
   (:gen-class))
 
-;; (require '[clojure.core.reducers :as r])
+;; (require '[cemerick.pomegranate :as pom]
+;;          '[cemerick.pomegranate.aether :as aether])
+
+;; (pom/add-dependencies :coordinates '[[incanter "1.9.2"]]
+;;                              :repositories (merge aether/maven-central
+;;                                                   {"clojars" "https://clojars.org/repo"}))
+
+;; (pom/add-dependencies :coordinates '[[clj-antlr "0.2.14"]]
+;;                              :repositories (merge aether/maven-central
+;;                                                   {"clojars" "https://clojars.org/repo"}))
+
+(require '[clojure.core.reducers :as r])
 (require ['clj-antlr.core :as 'antlr])
 
-;;(def myparse (antlr/parser "grammars/Day20.g4"))
-(def myparse (antlr/parser "grammars/Day17.g4"))
+(def myparse (antlr/parser "grammars/Day20.g4"))
+;;(def myparse (antlr/parser "grammars/Day17.g4"))
 
-;;(def t (myparse "NS(NSE|WSE)"))
+;; map myparse over list of inputs seems to quash the compiler in compiler mode
+(print (myparse "NSEW"))
+(def t (map myparse (list "NS(NSE|WSE)")))
 
 (def s 3)
 
-(def s2 (myparse "x=581, y=396..399"))
+;;(def s2 (myparse "x=581, y=396..399"))
 
+
+
+(defn -main [& args]
+  (print "hello world\n"))
 
