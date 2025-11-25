bf9072ddbb834ca9766d80d66b361813ddd7f663
terry <terry.cadd@gmail.com>
2025-11-25 14:01:34 +0000

    compilation takes too long
    running program interpreted in csi as compilation too long

--- day20/lisp/old/build-lisp2-prog.sh
+++ day20/lisp/old/build-lisp2-prog.sh
@@ -4,7 +4,11 @@ cat lisp2-header.scm > lisp2-prog.scm
 cat lisp2-out.scm >> lisp2-prog.scm
 cat lisp2-footer.scm >> lisp2-prog.scm
 
+echo "compiling lisp2-prog"
+
 csc -O3 lisp2-prog.scm -o lisp2-prog 
+
+echo "compiled - now run lisp2-prog"
 
 #echo "(load \"lisp2-prog.scm\")"
 #rlwrap csi
--- day20/lisp/old/lisp2-prog.scm
+++ day20/lisp/old/lisp2-prog.scm
@@ -8,6 +8,7 @@
 
 (define *max-count* 4352)
 (define *run-count* 0)
+(define *endpoints* #f)
 
 
 (define id
@@ -69,7 +70,8 @@
 
 ;;
 (define (run)
-  (t1 (list (make-point 0 0))))
+  (set! *endpoints* (t1 (list (make-point 0 0)))))
+
 (define t0 #f)
 (define t1 #f)
 (define t2 #f)
@@ -53964,3 +53966,32 @@
  (set! pts (t2 pts))
  (set! pts (t3 pts))
  pts))
+
+;; just run it - get idea of run time
+(run)
+
+;; report all open doors
+(call-with-output-file "lisp2-prog-open-doors.scm"
+  (lambda (port)
+    (format port "(define *open-doors* '("
+    (hash-table-for-each *doors* (lambda (k v)
+				   (format port "(~a ~a)~%" (point-x k) (point-y k))))
+    (format port "))~%"))))
+
+(format #t "recorded *open-doors* in lisp2-prog-open-doors.scm~%")
+
+;; 
+(call-with-output-file "lisp2-prog-end-points.scm"
+  (lambda (port)
+    (format port "(define *endpoints* '("
+	    (let loop ((pts *endpoints*))
+	      (cond
+	       ((null? pts) #f)
+	       (#t (let ((pt (car pts)))
+		     (format port "(~a ~a)~%" (point-x pt) (point-y pt))
+		     (loop (cdr pts)))))))
+    (format port "))~%")))
+
+(format #t "recorded *endpoints* in lisp2-prog-end-points.scm~%")
+
+    
